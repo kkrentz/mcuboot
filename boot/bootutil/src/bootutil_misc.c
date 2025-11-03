ec4d27872546c3dab282974a8cbcb417b7ffcf22
kkrentz <konrad.krentz@gmail.com>
2025-11-03 16:33:21 +0100

    bootutil: Provide for accessing TLVs of MCUboot's own image
    Normally, MCUboot does not access its own image. However, in the context of
    DICE, the DICE TLV imparts configuration data to Layer 0.

--- boot/bootutil/src/bootutil_misc.c
+++ boot/bootutil/src/bootutil_misc.c
@@ -5,6 +5,7 @@
  * Copyright (c) 2016-2019 JUUL Labs
  * Copyright (c) 2019-2020 Arm Limited
  * Copyright (c) 2025 Nordic Semiconductor ASA
+ * Copyright (c) 2025 Siemens AG
  *
  * Original license:
  *
@@ -459,6 +460,11 @@ boot_initialize_area(struct boot_loader_state *state, int flash_area)
         out_num_sectors = &state->scratch.num_sectors;
 #endif
 #endif
+#if MCUBOOT_USING_OWN_IMAGE
+    } else if (flash_area == FLASH_AREA_BOOTLOADER) {
+        out_sectors = state->bootloader.sectors;
+        out_num_sectors = &state->bootloader.num_sectors;
+#endif
     } else {
         return BOOT_EFLASH;
     }
@@ -654,6 +660,9 @@ boot_read_sectors(struct boot_loader_state *state, struct boot_sector_buffer *se
 #if MCUBOOT_SWAP_USING_SCRATCH
     state->scratch.sectors = sectors->scratch;
 #endif
+#if MCUBOOT_USING_OWN_IMAGE
+    state->bootloader.sectors = sectors->bootloader;
+#endif
 #endif
 #else
     (void)sectors;
@@ -677,6 +686,13 @@ boot_read_sectors(struct boot_loader_state *state, struct boot_sector_buffer *se
         return BOOT_EFLASH;
     }
 #endif
+#endif
+
+#if MCUBOOT_USING_OWN_IMAGE
+    rc = boot_initialize_area(state, FLASH_AREA_BOOTLOADER);
+    if (rc != 0) {
+        return BOOT_EFLASH;
+    }
 #endif
 
     BOOT_WRITE_SZ(state) = boot_write_sz(state);
