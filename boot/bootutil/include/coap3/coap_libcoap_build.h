0253478fa55c965a50d2e94bf9312d9dce49c61a
kkrentz <konrad.krentz@gmail.com>
2025-11-03 08:02:42 +0100

    ext: Add libcoap as a submodule
    We are not really using libcoap, but its CBOR and TinyDICE utils for
    the time being. Ultimately, we should converge on a common CBOR library.

---
+++ boot/bootutil/include/coap3/coap_libcoap_build.h
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2025 Siemens AG
+ */
+
+#ifndef COAP_LIBCOAP_BUILD_H_
+#define COAP_LIBCOAP_BUILD_H_
+
+#include "oscore-ng/oscore_ng_ecc_curve.h"
+#include "oscore-ng/oscore_ng_sha_256.h"
+#include "oscore-ng/oscore_ng_cbor.h"
+#include "oscore-ng/oscore_ng_tiny_dice.h"
+
+#endif /* COAP_LIBCOAP_BUILD_H_ */
